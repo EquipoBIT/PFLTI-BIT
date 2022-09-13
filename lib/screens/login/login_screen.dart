fbae4b565b151f58df297bb1b48c94a60adc5715
adrianutec2 <adrian.hitateguy@estudiantes.utec.edu.uy>
2022-09-13 08:56:37 -0300

    se modifican login screen y login screen test

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -72,7 +72,7 @@ class LoginScreen extends StatelessWidget {
                       height: 60,
                     ),
                     const SizedBox(height: 16),
-                    _GoogleLoginButton(),
+                    const GoogleLoginButton(),
                   ],
                 ),
               ),
@@ -82,76 +82,9 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-/*
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false,
-      child: BlocListener<AuthBloc, AuthState>(
-          listener: (context, state) {
-            debugPrint("AuthBloc BlocListener");
-            if (state.status == AuthStatus.unauthenticated) {
-              Timer(
-                const Duration(seconds: 1),
-                () => Navigator.of(context).pushNamedAndRemoveUntil(
-                  LoginScreen.routeName,
-                  ModalRoute.withName('/login'),
-                ),
-              );
-            } else if (state.status == AuthStatus.authenticated) {
-              Timer(
-                const Duration(seconds: 1),
-                () => Navigator.of(context).pushNamed(TermsScreen.routeName),
-              );
-            }
-          },
-          child: Scaffold(
-            body: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Align(
-                alignment: const Alignment(0, -1 / 3),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        color: Colors.black,
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 20,
-                        ),
-                        child: Text(
-                          'UTEC Gestion de EDTs',
-                          style:
-                              Theme.of(context).textTheme.headline2!.copyWith(
-                                    color: Colors.white,
-                                  ),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      Image.asset(
-                        'assets/images/logo_utec.png',
-                        height: 180,
-                      ),
-                      const SizedBox(height: 16),
-                      Image.asset(
-                        'assets/images/logo_bit.png',
-                        height: 60,
-                      ),
-                      const SizedBox(height: 16),
-                      _GoogleLoginButton(),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          )),
-    );
-  }
-}
-*/
+class GoogleLoginButton extends StatelessWidget {
+  const GoogleLoginButton({super.key});
 
-class _GoogleLoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
--- test/screens/home/login_screen_test.dart
+++ test/screens/home/login_screen_test.dart
@@ -1,26 +1,18 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pfltibit/blocs/auth/auth_bloc.dart';
 import 'package:pfltibit/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:pfltibit/repositories/repositories.dart';
-
-class MockAuthRepository extends Mock implements AuthRepository {}
 
 void main() {
   group('LoginScreen', () {
-    late AuthRepository authRepository;
+    setUp(() async {});
 
-    setUp(() {
-      authRepository = MockAuthRepository();
-      when(
-        () => authRepository.logInWithGoogle(),
-      ).thenAnswer((_) async {});
-    });
+    Widget makeTestableWidget({required Widget child}) {
+      return MaterialApp(home: child);
+    }
 
     testWidgets('Testing LoginScreen', (tester) async {
-      await tester.pumpWidget(const LoginScreen());
+      GoogleLoginButton page = const GoogleLoginButton();
+      await tester.pumpWidget(makeTestableWidget(child: page));
       expect(find.text('LOG IN WITH UTEC'), findsOneWidget);
     });
   });
