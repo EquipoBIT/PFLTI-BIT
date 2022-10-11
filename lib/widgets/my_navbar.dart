604548baa039762afbe4793f8c88ed1e60eb344a
adrianutec2 <adrian.hitateguy@estudiantes.utec.edu.uy>
2022-10-10 21:09:45 -0300

    implementacion de cloud functions start y strop edt

--- lib/widgets/my_navbar.dart
+++ lib/widgets/my_navbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../blocs/blocs.dart';
 import '/models/models.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 class MyNavBar extends StatelessWidget {
   final String screen;
@@ -19,7 +20,9 @@ class MyNavBar extends StatelessWidget {
       color: Colors.black,
       child: Container(
         height: 70,
-        child: (screen == '/edt') ? AddToEnUsoNavBar(edt: edt!) : HomeNavBar(),
+        child: (screen == '/edt')
+            ? AddToEnUsoNavBar(edt: edt!)
+            : const HomeNavBar(),
       ),
     );
   }
@@ -37,13 +40,13 @@ class HomeNavBar extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         IconButton(
-          icon: Icon(Icons.home, color: Colors.white),
+          icon: const Icon(Icons.home, color: Colors.white),
           onPressed: () {
             Navigator.pushNamed(context, '/');
           },
         ),
         IconButton(
-          icon: Icon(Icons.person, color: Colors.white),
+          icon: const Icon(Icons.person, color: Colors.white),
           onPressed: () {
             Navigator.pushNamed(context, '/profile');
           },
@@ -67,38 +70,28 @@ class AddToEnUsoNavBar extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         IconButton(
-          icon: Icon(Icons.power_settings_new, color: Colors.white),
+          icon: const Icon(Icons.power_settings_new, color: Colors.white),
           onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                    'EDT ON! Se descuenta tiempo de consumo hasta que Ud lo Detenga o se Agote el saldo.'),
-              ),
-            );
+            startEDT(context, edt.edtNombre);
           },
         ),
         IconButton(
-          icon: Icon(Icons.power_off, color: Colors.white),
+          icon: const Icon(Icons.power_off, color: Colors.white),
           onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                    'EDT OFF! Se congela el tiempo de consumo hasta que Ud encienda nuevamente el EDT.'),
-              ),
-            );
+            stopEDT(context, edt.edtNombre);
           },
         ),
         BlocBuilder<EnUsoBloc, EnUsoState>(
           builder: (context, state) {
             if (state is EnUsoLoading) {
-              return CircularProgressIndicator();
+              return const CircularProgressIndicator();
             }
             if (state is EnUsoLoaded) {
               return IconButton(
-                icon: Icon(Icons.handyman, color: Colors.white),
+                icon: const Icon(Icons.handyman, color: Colors.white),
                 onPressed: () {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
+                    const SnackBar(
                       content: Text('Added to your EnUsoList!'),
                     ),
                   );
@@ -106,10 +99,60 @@ class AddToEnUsoNavBar extends StatelessWidget {
                 },
               );
             }
-            return Text('Something went wrong!');
+            return const Text('Something went wrong!');
           },
         ),
       ],
     );
+  }
+
+// https://firebase.flutter.dev/docs/functions/usage/
+
+  Future<void> stopEDT(BuildContext context, String edtNombre) async {
+    try {
+      final result = await FirebaseFunctions.instance
+          .httpsCallable('stopEDT2')
+          .call(<String, dynamic>{'instanceName': edtNombre});
+ 
+// ¡¡¡¡¡¡¡¡¡¡¡¡¡
+// OJO HAY QUE SACAR ESTE MSG para el screen y no ponerlo en el metodo async!!!
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+              'EDT OFF! Se congela el tiempo de consumo hasta que Ud encienda nuevamente el EDT.'),
+        ),
+      );
+      debugPrint("result: ${result.data}");
+    } on FirebaseFunctionsException catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+              'ERROR EN EDT OFF!!!!'),
+        ),
+      );
+      debugPrint(error.code);
+      debugPrint(error.details);
+      debugPrint(error.message);
+    }
+  }
+
+  Future<void> startEDT(BuildContext context, String edtNombre) async {
+    try {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+              'EDT ON! Se descuenta tiempo de consumo hasta que Ud lo Detenga o se Agote el saldo.'),
+        ),
+      );
+      final result = await FirebaseFunctions.instance
+          .httpsCallable('startEDT2')
+          .call(<String, dynamic>{'instanceName': edtNombre});
+      debugPrint("result: ${result.data}");
+    } on FirebaseFunctionsException catch (error) {
+      debugPrint(error.code);
+      debugPrint(error.details);
+      debugPrint(error.message);
+    }
   }
 }
